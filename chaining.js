9ddec898d2e270590e5c62d94578cd9a281229c6
Abbie Holder <abigail.c.holder@gmail.com>
2016-02-22 18:11:11 -0600

    cleaned up code

--- chaining.js
+++ chaining.js
@@ -1,15 +1,13 @@
-var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8]
-.sort(function(first, second) { 
-  return first - second; })
-.reverse()
-.filter(function(element, index, array) {
-  if (element < 19) {
-    return element
-  }
-})
-.map(function(index) {return index * 1.5 - 1})
-.reduce(function(previousValue, currentValue, index, array) {
-  return previousValue + currentValue;
-})
+var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-console.log(integers);
+var chain = integers
+.sort(function(a, b) { 
+  return a - b; })
+.reverse()
+.filter(function(num) {
+  return num < 19})
+.map(function(num) {return (num * 1.5) - 1})
+.reduce(function(previousValue, currentValue) {
+  return previousValue + currentValue;})
+
+console.log(chain);
